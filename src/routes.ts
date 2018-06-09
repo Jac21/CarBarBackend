deb1154d4b550290711ab11b9fcf796ba6831657
Jeremy Cantu <mail@jeremycantu.com>
2018-06-09 13:54:02 -0500

    init commit w/ typeorm and express backend sample code

---
+++ src/routes.ts
@@ -0,0 +1,43 @@
+import { postGetAllAction } from "./controller/PostGetAllAction";
+import { postGetByIdAction } from "./controller/PostGetByIdAction";
+import { postSaveAction } from "./controller/PostSaveAction";
+
+import { userGetAllAction } from "./controller/UserGetAllAction";
+import { userGetByIdAction } from "./controller/UserGetByIdAction";
+import { userSaveAction } from "./controller/UserSaveAction";
+
+/**
+ * All application routes.
+ */
+export const AppRoutes = [
+    {
+        path: "/posts",
+        method: "get",
+        action: postGetAllAction
+    },
+    {
+        path: "/posts/:id",
+        method: "get",
+        action: postGetByIdAction
+    },
+    {
+        path: "/posts",
+        method: "post",
+        action: postSaveAction
+    },
+    {
+        path: "/users",
+        method: "get",
+        action: userGetAllAction
+    },
+    {
+        path: "/user/:id",
+        method: "get",
+        action: userGetByIdAction
+    },
+    {
+        path: "/users",
+        method: "post",
+        action: userSaveAction
+    }
+];
