e76de3020c51f393183d894b8eb13498ca45df20
Ritika Mehta <mehtarit@umich.edu>
2018-06-10 14:21:20 -0500

    Added working example for geoLocation and for sending messages to users

--- src/controller/SendMessage.ts
+++ src/controller/SendMessage.ts
@@ -9,15 +9,35 @@ export async function sendMessage(request: Request, response: Response) {
 
 	const client = require('twilio')(accountSid, authToken);
 
-	client.messages
+	    // get a user repository to perform operations with post
+    const userRepository = getManager().getRepository(User);
+
+    // load a user by a given post id
+    const users = await userRepository.find();
+
+try{
+
+	users.forEach(function(user){
+
+   	client.messages
   	.create({
      body: 'Hi Ritika, this is Natasha from CarBar', // this is message body for texting
      from: '+15037147388', //This is CarBar's Twilio number
      //Add media to a message 
      //mediaUrl: 'https://www.alivenetwork.com/images/extrabandpics/thecarbar4.jpg',
-     to: '+17348349568' //Customer's number
+     to: user.PHONE_NUMBER //Customer's number
    })
   .then(message => console.log(message.sid))
   .done();
-    response.send('Message Sent');
+       console.log(user.PHONE_NUMBER);
+   })
+
+    response.send('Messages Sent');
+
+}
+catch(ex)
+{
+	
+}
+
 }
--- src/controller/googleMaps.ts
+++ src/controller/googleMaps.ts
@@ -1,20 +1,29 @@
 import { Request, Response } from "express";
-import http = require('http');
 import { googleMapsApiKey } from "../sensitive";
 
 
 export async function googleMaps(request: Request, response: Response) {
 
-  var googleMapsClient = require('@google/maps').createClient({
-  key: googleMapsApiKey
+  const https = require('https');
+  const googleMapsQuery = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=driving&language=en-EN&key=AIzaSyCnz6owGzTO8iZ04YlOzbG1XBN1BH7qIoo';
+
+ // const distMatrix;
+  https.get(googleMapsQuery, (res) => {
+  console.log('statusCode:', res.statusCode);
+
+  var body = '';
+
+  res.on('data', function(chunk) {
+    body +=chunk;
   });
- 
-  var test = googleMapsClient.geocode({
-    address: '1600 Amphitheatre Parkway, Mountain View, CA'
-  }, function(err, res) {
-  if (!err) {
-     console.log(res.json.results);
-  }
+
+  res.on('end', function(){
+    var mapsResponse = JSON.parse(body);
+      console.log("Got a response: ", mapsResponse);
+      response.send(mapsResponse);
+    });
+
+}).on('error', (e) => {
+  console.error(e);
 });
-   response.send(test);
 }
