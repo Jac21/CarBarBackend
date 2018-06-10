3100d31437e3e3c71507d4d356b9c42658e98bfc
Ritika Mehta <mehtarit@umich.edu>
2018-06-10 14:33:21 -0500

    Changed to add a catch for all errors

--- src/controller/SendMessage.ts
+++ src/controller/SendMessage.ts
@@ -9,35 +9,24 @@ export async function sendMessage(request: Request, response: Response) {
 
 	const client = require('twilio')(accountSid, authToken);
 
-	    // get a user repository to perform operations with post
     const userRepository = getManager().getRepository(User);
-
-    // load a user by a given post id
     const users = await userRepository.find();
-
-try{
 
 	users.forEach(function(user){
 
    	client.messages
   	.create({
-     body: 'Hi Ritika, this is Natasha from CarBar', // this is message body for texting
+     body: 'Welcome to CarBar!', // this is message body for texting
      from: '+15037147388', //This is CarBar's Twilio number
      //Add media to a message 
-     //mediaUrl: 'https://www.alivenetwork.com/images/extrabandpics/thecarbar4.jpg',
+     mediaUrl: 'https://raw.githubusercontent.com/Jac21/carbar-frontend/master/public/img/V1.png',
      to: user.PHONE_NUMBER //Customer's number
    })
   .then(message => console.log(message.sid))
-  .done();
+  .catch(err => console.error(err));
        console.log(user.PHONE_NUMBER);
    })
 
     response.send('Messages Sent');
-
-}
-catch(ex)
-{
-	
-}
 
 }
